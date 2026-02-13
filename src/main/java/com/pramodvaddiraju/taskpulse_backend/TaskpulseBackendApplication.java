dd5a3fe768f3679dd8a629130d5488870b1fa59a
pramodvaddi <pramodvaddiraju@gmail.com>
2026-02-13 18:38:22 +0530

    initial commit

---
+++ src/main/java/com/pramodvaddiraju/taskpulse_backend/TaskpulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.taskpulse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskpulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskpulseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pramodvaddiraju/taskpulse_backend/TaskpulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.taskpulse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskpulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
