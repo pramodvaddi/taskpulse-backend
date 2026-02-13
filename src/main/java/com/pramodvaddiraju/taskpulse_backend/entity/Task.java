9faa72ebb14d9cc2a11c5fdfa508e1ed5f02f156
pramodvaddi <pramodvaddiraju@gmail.com>
2026-02-13 18:49:49 +0530

    added entity and mapped fields to database

---
+++ src/main/java/com/pramodvaddiraju/taskpulse_backend/entity/Task.java
@@ -0,0 +1,71 @@
+package com.pramodvaddiraju.taskpulse_backend.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime dueTime;
+    private String email;
+    private String status;
+
+    public Task(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDueTime() {
+        return dueTime;
+    }
+
+    public void setDueTime(LocalDateTime dueTime) {
+        this.dueTime = dueTime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
